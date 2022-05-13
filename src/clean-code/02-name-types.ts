767f599106afabc713d6d73ff6d011180443a8b2
unknown <orlinwilliams0077@gmial.com>
2022-05-12 19:39:15 -0600

    Anotaciones

---
+++ src/clean-code/02-name-types.ts
@@ -0,0 +1,59 @@
+(() => {
+
+    
+    const temperatureValues = [33.6, 12.34];
+
+    
+    const serverIp = '123.123.123.123';
+
+    
+    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    
+    const userEmails = users.map( user => user.email );
+
+    
+    const canJump = false;
+    const canRun = true;
+    const isEmptyItems = true;
+    const isLoading = false;
+
+    
+    const startTime = new Date().getTime();
+    //....
+    // 3 doritos después
+    //...
+    // Tiempo al final
+    const endTime = new Date().getTime() - startTime;
+
+
+    
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    
+    function getBookBysUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+    function getArea( side: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
+
+
+
+
